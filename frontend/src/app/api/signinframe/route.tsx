e1243ffc63f6f74f5bebb444ead3e7f1ad6d2518
okste1234 <okste1234@gmail.com>
2024-07-17 20:57:29 +0100

    FEAT: testing live

--- frontend/src/app/api/signinframe/route.tsx
+++ frontend/src/app/api/signinframe/route.tsx
@@ -4,67 +4,69 @@ import { NextRequest, NextResponse } from "next/server";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
     try {
-        const base64Image = await generateBase64Image();
+
+        const base64PriceImage = await generateBase64Image();
 
         return new NextResponse(`
           <!DOCTYPE html>
           <html>
             <head>
-              <title>Rap Mattaz Farcaster Frame</title>
-              <meta property="og:title" content="Rap Mattaz Farcaster Frame" />
+              <title>Price Feed Farcaster Frame</title>
+              <meta property="og:title" content="Price Feed Farcaster Frame" />
               <meta
                 property="og:image"
-                content="${envVars.hostUrl}/RapMattaz.png"
+                content=${envVars.hostUrl}/RapMattaz.png
               />
               <meta property="fc:frame" content="vNext" />
               <meta
                 property="fc:frame:image"
-                content="data:image/png;base64,${base64Image}"
+                content="data:image/png;base64,${base64PriceImage}"
               />
-              <meta property="fc:frame:button:1" content="sign in" />
-              <meta property="fc:frame:button:1:action" content="link" />
-              <meta property="fc:frame:button:1:target" content="${envVars.hostUrl}/signin" />
+              <meta
+                property="fc:frame:post_url"
+                content=${envVars.hostUrl}/api/signinframe
+              />
+              <meta property="fc:frame:button:1" content="Refresh" />
             </head>
             <body>
-              <p>Rap Mattaz Farcaster Frame</p>
+              <p>"Price Feed Farcaster Frame</p>
             </body>
           </html>
-        `, {
-            headers: { 'Content-Type': 'text/html' }
-        });
+    `);
     } catch (error) {
         return new NextResponse(`
           <!DOCTYPE html>
           <html>
             <head>
-              <title>Rap Mattaz Farcaster Frame</title>
-              <meta property="og:title" content="Rap Mattaz Farcaster Frame" />
+              <title>Price Feed Farcaster Frame</title>
+              <meta property="og:title" content="Price Feed Farcaster Frame" />
               <meta
                 property="og:image"
-                content="${envVars.hostUrl}/error.png"
+                content=${envVars.hostUrl}/error.png
               />
               <meta property="fc:frame" content="vNext" />
               <meta
                 property="fc:frame:image"
-                content="${envVars.hostUrl}/error.png"
+                content=${envVars.hostUrl}/error.png
               />
               <meta
                 property="fc:frame:post_url"
-                content="${envVars.hostUrl}"
+                content=${envVars.hostUrl}
               />
               <meta property="fc:frame:button:1" content="Reset" />
             </head>
             <body>
-              <p>Rap Mattaz Farcaster Frame</p>
+              <p>"Price Feed Farcaster Frame</p>
             </body>
           </html>
-        `, {
-            headers: { 'Content-Type': 'text/html' }
-        });
+        `);
     }
 }
 
-export async function POST(req: NextRequest, res: NextResponse): Promise<NextResponse> {
+export async function POST(
+    req: NextRequest,
+    res: NextResponse
+): Promise<Response> {
     return getResponse(req);
 }
 
